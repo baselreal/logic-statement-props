4529041d688adfdb143e03134e893fa10e6bba5d
baselreal <basriibrahim67067@gmail.com>
2022-11-21 11:18:52 +0700

    fix new logic

--- src/screens/array.jsx
+++ src/screens/array.jsx
@@ -1,191 +1,177 @@
-import React, { useState } from 'react'
-import {Component} from '../component/component.jsx'
+import React, { useState } from "react";
+import { Component } from "../component/component.jsx";
 
 function LogicArray() {
-
   //MAPS
 
-  const [maps, setMaps] = useState ()
-  const [handleMap, setHandleMap] = useState ()
-  
+  const [maps, setMaps] = useState();
+  const [handleMap, setHandleMap] = useState();
+
   const handleChangeMaps = (e) => {
-      setMaps(e.target.value)
-  }
+    setMaps(e.target.value);
+  };
 
   const handleClickMaps = () => {
-    const breaker = maps?.split(' ')
-    const wordBreak = breaker?.map(x => x = ' berhasil')
-    setHandleMap(wordBreak)
-  } 
+    const breaker = maps?.split(" ");
+    const wordBreak = breaker?.map((x) => (x = " berhasil"));
+    setHandleMap(wordBreak);
+  };
 
   //CONCAT
-  const [con, setCon] = useState ()
-  const [handleCon, setHandleCon] = useState ()
-  const word = [' array concat berhasil']
+  const [con, setCon] = useState();
+  const [handleCon, setHandleCon] = useState();
+  const word = [" array concat berhasil"];
 
   const handleChangeCon = (e) => {
-   setCon(e.target.value)
-  }
+    setCon(e.target.value);
+  };
 
   const handleClickCon = () => {
-    const breaker = con?.split(' ')
-    const wordBreak2 = breaker?.concat(word)
-    setHandleCon(wordBreak2)
-  } 
-
+    const breaker = con?.split(" ");
+    const wordBreak2 = breaker?.concat(word);
+    setHandleCon(wordBreak2);
+  };
 
   // FILTER
 
-  const [fil, setFil] = useState ()
-  const [handleFil, setHandleFil] = useState ()
+  const [fil, setFil] = useState();
+  const [handleFil, setHandleFil] = useState();
 
   const handleChangeFil = (e) => {
-   setFil(e.target.value)
-  }
+    setFil(e.target.value);
+  };
 
   const handleClickFil = () => {
-    const breaker = fil?.split(' ')
-    const wordBreak3 = breaker?.filter(val => val?.length === 3)
-    setHandleFil(wordBreak3[0])
-  } 
+    const breaker = fil?.split(" ");
+    const wordBreak3 = breaker?.filter((x) => x?.length < 3);
 
-  //FIND 
+    setHandleFil(wordBreak3);
+  };
 
-  const [find, setFind] = useState ()
-  const [handleFind, setHandleFind] = useState ()
+  //FIND
+
+  const [find, setFind] = useState();
+  const [handleFind, setHandleFind] = useState();
 
   const handleChangeFind = (e) => {
-   setFind(e.target.value)
-  }
+    setFind(e.target.value);
+  };
 
   const handleClickFind = () => {
-    const breaker = find?.split(' ')
-    const wordBreak4 = breaker?.find(x => x < 10)
-    setHandleFind(wordBreak4)
-  } 
+    const breaker = find?.split(" ");
+    const wordBreak4 = breaker?.find((x) => x < 10);
+    setHandleFind(wordBreak4);
+  };
 
   // INCLUDES
 
-  const [inc, setInc] = useState ()
-  const [handleInc, setHandleInc] = useState ()
-
+  const [inc, setInc] = useState();
+  const [handleInc, setHandleInc] = useState();
+  const cuy = "cuy";
 
   const handleChangeInc = (e) => {
-   setInc(e.target.value)
-  }
+    setInc(e.target.value);
+  };
 
   const handleClickInc = () => {
-    const breaker = inc?.split(' ')
-    const wordBreak5 = breaker?.includes('tidak ada input')
-    setHandleInc(wordBreak5[0])
-  }  
-  
-  
-  // JOIN 
-  
-  const [join, setJoin] = useState ([])
-  const [handleJoin, setHandleJoin] = useState ()
+    const wordBreak5 = `${inc.includes(cuy) ? "true" : "false"}`;
+    console.log(wordBreak5);
+    setHandleInc(wordBreak5);
+  };
 
+  // JOIN
+
+  const [join, setJoin] = useState([]);
+  const [handleJoin, setHandleJoin] = useState();
 
   const handleChangeJoin = (e) => {
-   setJoin(e.target.value)
-  }
+    setJoin(e.target.value);
+  };
 
   const handleClickJoin = () => {
-    const breaker = join?.split(' ')
-    const wordBreak6 = breaker?.join(['-'])
-    setHandleJoin(wordBreak6)
-  } 
+    const breaker = join?.split(" ");
+    const wordBreak6 = breaker?.join(["-"]);
+    setHandleJoin(wordBreak6);
+  };
 
   // PUSH
 
-  const [push, setPush] = useState ([])
-  const [handlePush, setHandlePush] = useState ()
-
+  const [push, setPush] = useState([]);
+  const [handlePush, setHandlePush] = useState();
 
   const handleChangePush = (e) => {
-   setPush(e.target.value)
-  }
+    setPush(e.target.value);
+  };
 
   const handleClickPush = () => {
-    const breaker = push?.split(' ')
-    const wordBreak7 = breaker?.push(['ayam'])
-    setHandlePush(wordBreak7)
-  } 
-
+    const breaker = push?.split(" ");
+    const wordBreak7 = breaker?.push(["ayam"]);
+    setHandlePush(wordBreak7);
+  };
 
   // REDUCE
-  const [reduce, setReduce] = useState ([])
-  const [handleReduce, setHandleReduce] = useState ()
-  const number = 200
-
+  const [reduce, setReduce] = useState([]);
+  const [handleReduce, setHandleReduce] = useState();
+  const number = 200;
 
   const handleChangeReduce = (e) => {
-    setReduce(e.target.value)
-  }
+    setReduce(e.target.value);
+  };
 
   const handleClickReduce = () => {
-    const breaker = reduce?.split(' ')
-    const wordBreak8 = breaker?.reduce((accumulator, currentValue)=>accumulator + currentValue, number)
-    setHandleReduce(wordBreak8)
-  } 
-
+    const breaker = reduce?.split(" ");
+    const wordBreak8 = breaker?.reduce((accumulator, currentValue) => accumulator + currentValue, number);
+    setHandleReduce(wordBreak8);
+  };
 
   // REVERSE
 
-  const [reverse, setReverse] = useState ([])
-  const [handleReverse, setHandleReverse] = useState ()
-
+  const [reverse, setReverse] = useState([]);
+  const [handleReverse, setHandleReverse] = useState();
 
   const handleChangeReverse = (e) => {
-    setReverse(e.target.value)
-  }
+    setReverse(e.target.value);
+  };
 
   const handleClickReverse = () => {
-    const breaker = reverse?.split(' ')
-    const wordBreak9 = breaker?.reverse()
-    setHandleReverse(wordBreak9)
-  } 
+    const breaker = reverse?.split(" ");
+    const wordBreak9 = breaker?.reverse();
+    setHandleReverse(wordBreak9);
+  };
 
+  // SORT
 
-  // SORT 
-
-  const [sort, setSort] = useState ([])
-  const [handleSort, setHandleSort] = useState ()
-
+  const [sort, setSort] = useState([]);
+  const [handleSort, setHandleSort] = useState();
 
   const handleChangeSort = (e) => {
-    setSort(e.target.value)
-  }
+    setSort(e.target.value);
+  };
 
   const handleClickSort = () => {
-    const breaker = sort?.split(' ')
-    const wordBreak10 = breaker?.sort()
-    setHandleSort(wordBreak10)
-  } 
+    const breaker = sort?.split(" ");
+    const wordBreak10 = breaker?.sort();
+    setHandleSort(wordBreak10);
+  };
 
-
-
-
-  
   return (
-    <div className='array'>
-        <h3>LOGIC STATEMENT ARRAY</h3>
+    <div className="array">
+      <h3>LOGIC STATEMENT ARRAY</h3>
 
-        <div className="component-array">
+      <div className="component-array">
         <Component label="name map (value yg kamu input + berhasil)" value={maps} handleChange={handleChangeMaps} handleClick={handleClickMaps} result={handleMap} />
         <Component label="name concat (value yg kamu input + array concat berhasil) " value={con} handleChange={handleChangeCon} handleClick={handleClickCon} result={handleCon} />
-        <Component label="name filter (value yg kamu input akan di filter)" value={fil} handleChange={handleChangeFil} handleClick={handleClickFil} result={handleFil} />
+        <Component label="name filter (value yg kamu input akan di masukan jika kata kurang dari 3)" value={fil} handleChange={handleChangeFil} handleClick={handleClickFil} result={handleFil} />
         <Component label="name find (masukan value < 10)" value={find} handleChange={handleChangeFind} handleClick={handleClickFind} result={handleFind} />
-        <Component label="name includes (hasilnya false)" value={inc} handleChange={handleChangeInc} handleClick={handleClickInc} result={handleInc} />
+        <Component label="name includes (jika tidak ada cuy, maka false dan sebaliknya)" value={inc} handleChange={handleChangeInc} handleClick={handleClickInc} result={handleInc} />
         <Component label="name join (value yg kamu input jika ada spasi diganti jadi -)" value={join} handleChange={handleChangeJoin} handleClick={handleClickJoin} result={handleJoin} />
-        <Component label="name push (menghitung value yg ada)" value={push} handleChange={handleChangePush} handleClick={handleClickPush} result={handlePush} />
+        <Component label="name push (menghitung value kata yg ada)" value={push} handleChange={handleChangePush} handleClick={handleClickPush} result={handlePush} />
         <Component label="name reduce (tulis berapapun angka akan muncul 200)" value={reduce} handleChange={handleChangeReduce} handleClick={handleClickReduce} result={handleReduce} />
         <Component label="name reverse (value yg kamu input akan terbalik)" value={reverse} handleChange={handleChangeReverse} handleClick={handleClickReverse} result={handleReverse} />
         <Component label="name sort (value yg kamu input akan di urutkan)" value={sort} handleChange={handleChangeSort} handleClick={handleClickSort} result={handleSort} />
-        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default LogicArray
+export default LogicArray;
--- src/screens/index.jsx
+++ src/screens/index.jsx
@@ -1,169 +1,157 @@
-import React from 'react'
-import {Component} from '../component/component.jsx'
-import { useState } from 'react'
-
-
+import React from "react";
+import { Component } from "../component/component.jsx";
+import { useState } from "react";
 
 export function Index() {
-
   //1.UPPERCASE
 
-  const [name, setName] = useState('')
-  const [resultName, setResultName] = useState('')
+  const [name, setName] = useState("");
+  const [resultName, setResultName] = useState("");
 
   const handleChangeName = (e) => {
-    setName(e.target.value)
-  
-  }
+    setName(e.target.value);
+  };
 
   const handleClickName = () => {
-    setResultName(name.toUpperCase())
-  }
+    setResultName(name.toUpperCase());
+  };
 
   //2.LOWERCASE
 
-  const [user, setUser] = useState('')
-  const [lowName, setLowName] = useState ('')
+  const [user, setUser] = useState("");
+  const [lowName, setLowName] = useState("");
 
   const handleSetUser = (e) => {
-    setUser(e.target.value)
-  }
+    setUser(e.target.value);
+  };
 
   const handleLowName = () => {
-    setLowName(user.toLowerCase())
-  }
+    setLowName(user.toLowerCase());
+  };
 
   //3.REPEAT
 
-  const [repName, setRepname] = useState('')
-  const [output, setOutput] = useState('')
+  const [repName, setRepname] = useState("");
+  const [output, setOutput] = useState("");
 
   const handleRepName = (e) => {
-    setRepname(e.target.value)
-  }
+    setRepname(e.target.value);
+  };
 
   const handleSetOutput = () => {
-    setOutput(repName.repeat(2))
-  }
-
+    setOutput(repName.repeat(2));
+  };
 
   //4.REPLACE
 
-  const [replName, setReplName] = useState('')
-  const [outputRepl, setOutputRepl] = useState('')
+  const [replName, setReplName] = useState("");
+  const [outputRepl, setOutputRepl] = useState("");
 
   const handleReplName = (e) => {
-    setReplName(e.target.value)
-  }
+    setReplName(e.target.value);
+  };
 
   const handleSetOutputRepl = () => {
-    setOutputRepl(replName.replaceAll('a', 'b'))
-  }
-
+    setOutputRepl(replName.replaceAll("a", "b"));
+  };
 
   //5.SUB STRING
 
-  const [subStringName, setSubStringName] = useState('')
-  const [outputSubString, setOutputSubString] = useState('')
+  const [subStringName, setSubStringName] = useState("");
+  const [outputSubString, setOutputSubString] = useState("");
 
   const handleSubStringName = (e) => {
-    setSubStringName(e.target.value)
-  }
+    setSubStringName(e.target.value);
+  };
 
   const handleSetOutputSubStringName = () => {
-    setOutputSubString(subStringName.substring(2))
-  }
-
+    setOutputSubString(subStringName.substring(2));
+  };
 
   //6.CODE POINT AT
 
-  const [cpaName, setCpaName] = useState('')
-  const [outputCpa, setOutputCpa] = useState('')
+  const [cpaName, setCpaName] = useState("");
+  const [outputCpa, setOutputCpa] = useState("");
 
   const handleCpaName = (e) => {
-    setCpaName(e.target.value)
-  }
+    setCpaName(e.target.value);
+  };
 
   const handleSetOutputCpaName = () => {
-    setOutputCpa(cpaName.codePointAt(2))
-  }
+    setOutputCpa(cpaName.codePointAt(2));
+  };
 
   //7. CONCAT
 
-  const [conName, setConName] = useState('')
-  const [outputCon, setOutputCon] = useState('')
-  const variable = "kamu nanya?"
+  const [conName, setConName] = useState("");
+  const [outputCon, setOutputCon] = useState("");
+  const variable = "kamu nanya?";
 
   const handleConName = (e) => {
-    setConName(e.target.value)
-  }
+    setConName(e.target.value);
+  };
 
   const handleSetOutputConName = () => {
-    setOutputCon(conName.concat('', variable))
-  }
+    setOutputCon(conName.concat("", variable));
+  };
 
+  // 8. AT
 
-  // 8. AT 
-
-  const [atName, setAtName] = useState('')
-  const [outputAt, setOutputAt] = useState('')
+  const [atName, setAtName] = useState("");
+  const [outputAt, setOutputAt] = useState("");
   const index = 5;
 
   const handleAtName = (e) => {
-    setAtName(e.target.value)
-  }
+    setAtName(e.target.value);
+  };
 
   const handleSetOutputAtName = () => {
-    setOutputAt(atName.at(index))
-  }
+    setOutputAt(atName.at(index));
+  };
 
-  // 9. Includes 
+  // 9. Includes
 
-  const [incName, setIncName] = useState('')
-  const [outputInc, setOutputInc] = useState('')
+  const [incName, setIncName] = useState("");
+  const [outputInc, setOutputInc] = useState("");
   const word = "hello";
 
   const handleIncName = (e) => {
-    setIncName(e.target.value)
-  }
+    setIncName(e.target.value);
+  };
 
   const handleSetOutputIncName = () => {
-     setOutputInc(`The word "${word}" ${incName.includes(word) ? 'is' : 'is not'} in the text`)
-  }
+    setOutputInc(`The word "${word}" ${incName.includes(word) ? "is" : "is not"} in the text`);
+  };
 
   // 10. prototype.padEnd
 
-  const [padName, setPadName] = useState('')
-  const [outputPad, setOutputPad] = useState('')
+  const [padName, setPadName] = useState("");
+  const [outputPad, setOutputPad] = useState("");
 
   const handlePadName = (e) => {
-    setPadName(e.target.value)
-  }
+    setPadName(e.target.value);
+  };
 
   const handleSetOutputPadName = () => {
-    setOutputPad(padName.padEnd(20, '*'))
-  }
+    setOutputPad(padName.padEnd(20, "*"));
+  };
 
   return (
+    <div className="logic-string">
+      <h3>LOGIC STATEMENT STRING</h3>
 
-  <div className='logic-string'>
-      
-     <h3>LOGIC STATEMENT STRING</h3>
-      
-    <div className='string'>
-    <Component label="name uppercase" value={name} handleChange={handleChangeName} handleClick={handleClickName} result={resultName} />
-    <Component label="name lowercase" value={user} handleChange={handleSetUser} handleClick={handleLowName} result={lowName} />
-    <Component label="name repeat" value={repName} handleChange={handleRepName} handleClick={handleSetOutput} result={output} />
-    <Component label="name replace" value={replName} handleChange={handleReplName} handleClick={handleSetOutputRepl} result={outputRepl} />
-    <Component label="name substring" value={subStringName} handleChange={handleSubStringName} handleClick={handleSetOutputSubStringName} result={outputSubString} />
-    <Component label="name code point at" value={cpaName} handleChange={handleCpaName} handleClick={handleSetOutputCpaName} result={outputCpa} />
-    <Component label="name concat" value={conName} handleChange={handleConName} handleClick={handleSetOutputConName} result={outputCon} />
-    <Component label="name at" value={atName} handleChange={handleAtName} handleClick={handleSetOutputAtName} result={outputAt} />
-    <Component label="name Includes" value={incName} handleChange={handleIncName} handleClick={handleSetOutputIncName} result={outputInc} />
-    <Component label="name PadEnd" value={padName} handleChange={handlePadName} handleClick={handleSetOutputPadName} result={outputPad} />
+      <div className="string">
+        <Component label="name uppercase (tulis huruf kecil akan jadi besar)" value={name} handleChange={handleChangeName} handleClick={handleClickName} result={resultName} />
+        <Component label="name lowercase (tulis huruf besar akan jadi kecil)" value={user} handleChange={handleSetUser} handleClick={handleLowName} result={lowName} />
+        <Component label="name repeat (mengulangi kata yg di input)" value={repName} handleChange={handleRepName} handleClick={handleSetOutput} result={output} />
+        <Component label="name replace (mengganti huruf a jadi b)" value={replName} handleChange={handleReplName} handleClick={handleSetOutputRepl} result={outputRepl} />
+        <Component label="name substring (akan dihitung 2 kata di value pertama)" value={subStringName} handleChange={handleSubStringName} handleClick={handleSetOutputSubStringName} result={outputSubString} />
+        <Component label="name code point at (kata diubah menjadi nomor) " value={cpaName} handleChange={handleCpaName} handleClick={handleSetOutputCpaName} result={outputCpa} />
+        <Component label="name concat (menambahkan kata, coba tanya)" value={conName} handleChange={handleConName} handleClick={handleSetOutputConName} result={outputCon} />
+        <Component label="name at (kata akan tampilkan sesuai index=5)" value={atName} handleChange={handleAtName} handleClick={handleSetOutputAtName} result={outputAt} />
+        <Component label="name Includes (kata hello, harus ada di input)" value={incName} handleChange={handleIncName} handleClick={handleSetOutputIncName} result={outputInc} />
+        <Component label="name PadEnd (jika string kurang dari 20, maka sisanya akan di tampilkan menjadi bintang *)" value={padName} handleChange={handlePadName} handleClick={handleSetOutputPadName} result={outputPad} />
+      </div>
     </div>
-
-      
-  </div>
-  )
+  );
 }
